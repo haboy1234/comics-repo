9991d7c8723f91ec5a9efc0765a88245a485fb77
clau <claudmatt@gmail.com>
2024-01-12 11:44:08 +0100

    Added Adsense Banners code

--- app/components/Ad.tsx
+++ app/components/Ad.tsx
@@ -1,5 +1,21 @@
 function Ad(): React.JSX.Element {
-	return <div></div>
+	return (
+		<div>
+			<script
+				async
+				src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4311249285901114'
+				crossOrigin='anonymous'></script>
+			{/* General React LeerComicsOnline Banner */}
+			<ins
+				className='adsbygoogle'
+				style={{ display: 'block' }}
+				data-ad-client='ca-pub-4311249285901114'
+				data-ad-slot='4606816034'
+				data-ad-format='auto'
+				data-full-width-responsive='true'></ins>
+			<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+		</div>
+	)
 }
 
 export default Ad
