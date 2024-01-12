2eb1b1a421fdf34339d9784d7738b9e5045fbc63
clau <claudmatt@gmail.com>
2024-01-12 11:13:45 +0100

    Added just one lazy load on series menu for performance optimitzation

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,18 +2,26 @@
 
 import Link from 'next/link'
 import useSeries from '../hooks/useSeries'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 function Menu(): React.JSX.Element {
 	const [series, filter, setFilter] = useSeries()
-
+	const [seriesToShow, setSeriesToShow] = useState<number>(50)
 	const search = useRef<HTMLInputElement>(null)
+	const list = useRef<HTMLUListElement>(null)
 
 	const updateSeries = (): void => {
 		setFilter(series)
 		setFilter(currentFiltered => {
 			return currentFiltered.filter(s => s.title.toLowerCase().includes(search.current!.value.toLowerCase()))
 		})
+	}
+
+	const detectScroll = (): void => {
+		if (list.current === null) return
+		if (list.current.scrollTop + list.current.clientHeight >= list.current.scrollHeight - 100) {
+			setSeriesToShow(1000)
+		}
 	}
 
 	return (
@@ -29,9 +37,13 @@ function Menu(): React.JSX.Element {
 			/>
 			<nav className='main-menu'>
 				<ul
+					ref={list}
+					onScroll={() => {
+						detectScroll()
+					}}
 					className='w-full overflow-y-auto overflow-x-clip max-w-64 pt-3 border-b-2 absolute top-[103px] bottom-[33px]'
 					style={{ backgroundColor: 'rgb(var(--black))', borderBottomColor: 'rgb(var(--dark-green))' }}>
-					{filter?.slice(0, 50).map((serie, index) => {
+					{filter?.slice(0, seriesToShow).map((serie, index) => {
 						return (
 							<li key={index}>
 								<Link
