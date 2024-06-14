a21afb41dbe78697eda54a2e74b9242cdbda56ca
clau <claudmatt@gmail.com>
2024-06-14 23:20:31 +0200

    Youtube try

--- app/components/Youtube.tsx
+++ app/components/Youtube.tsx
@@ -1,29 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import YouTubePlayer from 'youtube-player'
 
 export default function Youtube(): React.JSX.Element {
-	const websites: string[] = ['alverno.edu/', 'bates.edu/', 'uchicago.edu/', 'risd.edu/', 'vcu.edu/']
+	// const websites: string[] = ['alverno.edu/', 'bates.edu/', 'uchicago.edu/', 'risd.edu/', 'vcu.edu/']
 	const ids: string[] = ['ylssgHLVZaE']
 	const [showButton, setShowButton] = useState('block')
+	const player = YouTubePlayer('video-player')
 
 	const onClick = async (): Promise<void> => {
 		setShowButton('hidden')
 	}
 
+	useEffect(() => {
+		player.loadVideoById(ids[0])
+		player.mute()
+		player.setVolume(0)
+	}, [])
+
 	return (
 		<>
 			<div style={{ opacity: 0, display: showButton }}>Haz clic aquí para empezar a leer</div>
-			<iframe
+			<div
 				onClick={() => {
 					onClick()
 				}}
 				id='video-player'
-				style={{ position: 'absolute', top: '-3000px', display: showButton }}
-				allowFullScreen={false}
-				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-				referrerPolicy='strict-origin-when-cross-origin'
-				width='640'
-				height='360'
-				src={`https://www.youtube.com/embed/${ids[0]}?enablejsapi=1&amp;origin=https%3A%2F%2F${websites[0]}&amp`}></iframe>
+				style={{ position: 'absolute', top: '-3000px', display: showButton }}></div>
 		</>
 	)
 }
